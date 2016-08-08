5671169fc6c6242adf0cc8f66543a37551367607
Starbugstone <pint2stone@gmail.com>
2016-08-08 11:55:15 +0200

    Update on the logo
    Just a logo update

--- navBar.php
+++ navBar.php
@@ -6,7 +6,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Logo</a>
+      <a class="navbar-brand" href="index.php"><img src="img/logoAS-small.png" alt="AiderSante" width="25px" height="25px"></a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
